0dca5f63fdbbe5020a270707af5fd57a683602a1
Mandrake02 <andrea.mandrache@gmail.com>
2026-03-26 11:17:40 +0100

    Fine prima ora lezione 26-03-26

--- examples/examples_3_AI/Vector2D.cpp
+++ examples/examples_3_AI/Vector2D.cpp
@@ -1,63 +1,53 @@
 #include "Vector2D.h"
+#include <cmath> 
+#include <iomanip>
+#include <sstream>
 
-#include <cmath>
+// Initialize static member
+size_t Vector2D::_vector_count = 0;
 
-Vector2D::Vector2D()
-    : _x(0.0), _y(0.0), _label("vec_" + std::to_string(++_vector_count)) {}
+Vector2D::Vector2D() : _x(0.0), _y(0.0), _label("") {}
 
-Vector2D::Vector2D(double x, double y)
-    : _x(x), _y(y), _label("vec_" + std::to_string(++_vector_count)) {}
-
-double Vector2D::x() const {
-    return _x;
+Vector2D::Vector2D(double x, double y) : _x(x), _y(y) {
+  _label = "vec_" + std::to_string(++_vector_count);
 }
 
-double Vector2D::y() const {
-    return _y;
+Vector2D::Vector2D(const std::string label, double x, double y)
+    : _label(label), _x(x), _y(y) {}
+
+double Vector2D::x() const { return _x; }
+
+double Vector2D::y() const { return _y; }
+
+std::string Vector2D::label() const { return _label; }
+
+double Vector2D::norm() const { return std::hypot(_x, _y); }
+
+double Vector2D::distance_to(const Vector2D &other) const {
+  return std::hypot(_x - other._x, _y - other._y);
 }
 
-double Vector2D::norm() const {
-    return std::sqrt(_x * _x + _y * _y);
+Vector2D Vector2D::operator+(const Vector2D &other) const {
+  return Vector2D(_x + other._x, _y + other._y);
 }
 
-double Vector2D::distance_to(const Vector2D& other) const {
-    const double dx = _x - other._x;
-    const double dy = _y - other._y;
-    return std::sqrt(dx * dx + dy * dy);
-}
-
-Vector2D Vector2D::operator+(const Vector2D& other) const {
-    return Vector2D(_x + other._x, _y + other._y);
-}
-
-Vector2D Vector2D::operator-(const Vector2D& other) const {
-    return Vector2D(_x - other._x, _y - other._y);
+Vector2D Vector2D::operator-(const Vector2D &other) const {
+  return Vector2D(_x - other._x, _y - other._y);
 }
 
 Vector2D Vector2D::operator*(double scalar) const {
-    return Vector2D(_x * scalar, _y * scalar);
+  return Vector2D(_x * scalar, _y * scalar);
 }
 
 std::string Vector2D::to_csv_row() const {
-    std::string escaped_label;
-    escaped_label.reserve(_label.size());
-
-    for (char c : _label) {
-        if (c == '"') {
-            escaped_label += "\"\"";
-        } else {
-            escaped_label += c;
-        }
-    }
-
-    return "\"" + escaped_label + "\"," + std::to_string(_x) + "," + std::to_string(_y);
+  std::ostringstream oss;
+  oss << _label << ',' << std::setprecision(10) << _x << ',' << _y;
+  return oss.str();
 }
 
-std::string Vector2D::to_csv_header() {
-    return "label,x,y";
-}
+std::string Vector2D::to_csv_header() { return "label,x,y"; }
 
-std::ostream& operator<<(std::ostream& os, const Vector2D& v) {
-    os << "(" << v.x() << ", " << v.y() << ")";
-    return os;
+std::ostream &operator<<(std::ostream &os, const Vector2D &v) {
+  os << "[" << v.label() << "] (" << v.x() << ", " << v.y() << ")";
+  return os;
 }
---
+++ examples/examples_3_AI/example_3.cpp
@@ -0,0 +1,7 @@
+#include "Vector2D.h"
+#include <iostream>
+
+int main() {
+  
+  return 0;
+}
